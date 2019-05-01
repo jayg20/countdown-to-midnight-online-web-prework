d0c045e9289400606b988c87c91eb9deae903769
James Gomez <james.gomez320@yahoo.com>
2019-05-01 15:29:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+number = 10
 
 def countdown
+  while number < 11
+  number -= 1
+  puts "#{number} SECONDS(S)"
+else
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep 
+  sleep(5)
 end
